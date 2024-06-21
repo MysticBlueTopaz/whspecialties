ce93ed96b200b06408f92e0a39c078be6358f391
MysticBlueTopaz <mysticBlueTopaz@gmail.com>
2024-06-20 20:18:22 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+ <div class="menu">This the the menu</div>
+ <div class="main_component"></div>
+</template>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+<style scoped>
+
+</style>
